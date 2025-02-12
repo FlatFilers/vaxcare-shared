65c55bda27de1141da953e30c0466444636df91c
syshinnn <seungyeon.009@gmail.com>
2025-02-12 09:43:54 -0600

    change file structure

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import FlatfileListener, { Listener } from "@flatfile/listener";
 import { configureSpace } from "@flatfile/plugin-space-configure";
 import { ExcelExtractor } from "@flatfile/plugin-xlsx-extractor";
-import { instrumentRequests } from "../../support/instrument.requests";
-import "../../support/requests/records/global.collect.macros";
+import { instrumentRequests } from "../support/instrument.requests";
 import workbook from "./blueprints/workbooks/workbook";
 import { bulkRecordHook, FlatfileRecord } from "@flatfile/plugin-record-hook";
 
